6fe465bb741863e89afbcbc11573b1a4fd1321cd
Steven <s@ineva.cn>
2025-11-24 15:38:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -ex
 
 RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
 RUN_XTERM=${RUN_XTERM:-yes}
+VNC_SERVER_HOST=${VNC_SERVER_HOST:-localhost}
+VNC_SERVER_PORT=${VNC_SERVER_PORT:-5900}
 
 case $RUN_FLUXBOX in
   false|no|n|0)
@@ -15,5 +17,9 @@ case $RUN_XTERM in
     rm -f /app/conf.d/xterm.conf
     ;;
 esac
+
+cp /app/websockify.conf /app/conf.d/websockify.conf
+sed -i "s/localhost/$VNC_SERVER_HOST/g" /app/conf.d/websockify.conf
+sed -i "s/5900/$VNC_SERVER_PORT/g" /app/conf.d/websockify.conf
 
 exec supervisord -c /app/supervisord.conf
